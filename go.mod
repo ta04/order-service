42c456fc381812bf7634ac500e420050a2a18762
SleepingNext <veroskevin8@gmail.com>
2020-03-11 12:00:45 +0700

    Integrate with JWT

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/SleepingNext/order-service
 go 1.13
 
 require (
+	github.com/SleepingNext/auth-service v0.0.0-20200310114554-2079cc787758
 	github.com/coreos/etcd v3.3.18+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/mock v1.3.1 // indirect
@@ -14,7 +15,6 @@ require (
 	github.com/miekg/dns v1.1.27 // indirect
 	github.com/nats-io/nats-server/v2 v2.1.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/spf13/cobra v0.0.6 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20200122045848-3419fae592fc // indirect
 	go.uber.org/zap v1.13.0 // indirect
 	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d // indirect
@@ -23,6 +23,4 @@ require (
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	golang.org/x/tools v0.0.0-20191216173652-a0e659d51361 // indirect
-	google.golang.org/genproto v0.0.0-20191216164720-4f79533eabd1 // indirect
-	google.golang.org/grpc v1.26.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,9 +3,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/SleepingNext/order-service/repository/postgres"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
 	"log"
 
+	authPB "github.com/SleepingNext/auth-service/proto"
 	"github.com/SleepingNext/order-service/database"
 	"github.com/SleepingNext/order-service/handler"
 	orderPB "github.com/SleepingNext/order-service/proto"
@@ -17,6 +23,7 @@ func main() {
 	// Create a new service
 	s := micro.NewService(
 		micro.Name("com.ta04.srv.order"),
+		micro.WrapHandler(AuthWrapper),
 	)
 
 	// Initialize the service
@@ -46,5 +53,28 @@ func main() {
 	err = s.Run()
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, res interface{}) error {
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in the request")
+		}
+
+		token := meta["Token"]
+		log.Println("authenticating with token: ", token)
+
+		// Validate the token
+		authClient := authPB.NewAuthServiceClient("go.micro.srv.auth", client.DefaultClient)
+		_, err := authClient.ValidateToken(context.Background(), &authPB.Token{
+			Token: token,
+		})
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, res)
+		return err
 	}
 }
