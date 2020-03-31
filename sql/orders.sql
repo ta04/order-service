432cfd4be38c12e19aa1ef7b2f6b7d3bfd1161cb
SleepingNext <veroskevin8@gmail.com>
2020-03-31 15:03:08 +0700

    Add sql file

---
+++ sql/orders.sql
@@ -0,0 +1,100 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.7 (Ubuntu 11.7-1.pgdg18.04+1)
+-- Dumped by pg_dump version 11.7 (Ubuntu 11.7-1.pgdg18.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: orders; Type: DATABASE; Owner: sleepingnext
+--
+
+CREATE DATABASE orders;
+
+ALTER DATABASE orders OWNER TO sleepingnext;
+
+--
+-- Name: orders; Type: TABLE; Schema: public; Owner: sleepingnext
+--
+
+\connect orders;
+CREATE TABLE public.orders (
+    id bigint NOT NULL,
+    product_id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    status character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.orders OWNER TO sleepingnext;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: sleepingnext
+--
+
+CREATE SEQUENCE public.orders_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.orders_id_seq OWNER TO sleepingnext;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: sleepingnext
+--
+
+ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
+
+
+--
+-- Name: orders id; Type: DEFAULT; Schema: public; Owner: sleepingnext
+--
+
+ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.orders_id_seq'::regclass);
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: sleepingnext
+--
+
+COPY public.orders (id, product_id, user_id, status) FROM stdin;
+\.
+
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: sleepingnext
+--
+
+SELECT pg_catalog.setval('public.orders_id_seq', 1, false);
+
+
+--
+-- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: sleepingnext
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
